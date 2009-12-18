5809b2a07a75a02ec7be9904ed7232bf14901b99
Brian Gianforcaro <b.gianfo@gmail.com>
2009-12-17 21:00:53 -0500

    Inital repo layout

---
+++ include/hmipv6_support.h
@@ -0,0 +1,21 @@
+/**
+ * File: hmipv6_support.h
+ *
+ * Description: Support file for Hierarchal Mobile IPv6
+ *              Data structures and macro's for implementation.
+ *
+ * Author: Brian Gianforcaro (b.gianfo@gmail.com)
+ */
+
+#ifndef _HMIPV6_SUPPORT_H_
+#define _HMIPV6_SUPPORT_H_
+
+#if defined (__cplusplus)
+extern "C" {
+#endif
+
+#if defined (__cplusplus)
+} /* end of extern "C" { */
+#endif
+
+#endif 
